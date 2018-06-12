49a0391681d3373ac87830824d5efd612b0d3032
Berke Boz <berkeboz96@gmail.com>
2018-06-11 23:39:24 -0700

    Added Levels, Difficulty and Skin Selection

--- Ant Lion Redesigned/Assets/Scripts/globalState.cs
+++ Ant Lion Redesigned/Assets/Scripts/globalState.cs
@@ -8,7 +8,8 @@ public class globalState : MonoBehaviour {
     public Difficulty difficulty;
 
     public enum Skins {skin1, skin2, skin3, skin4, skin5 }
-    
+    public enum LevelNames {lvl1,lvl2,lvl3, lvl4 }
+    public LevelNames level;
 
     // Use this for initialization
     void Start()
--- Ant Lion Redesigned/Assets/Scripts/platformGenerator.cs
+++ Ant Lion Redesigned/Assets/Scripts/platformGenerator.cs
@@ -10,10 +10,15 @@ public class platformGenerator : MonoBehaviour {
 
     ArrayList platformList = new ArrayList();
 
-
+    private GameObject stateObj;
+    private globalState state;
 
 	void Start () {
         basePlatform = this.gameObject;
+        stateObj = GameObject.Find("GlobalState");
+        state = stateObj.GetComponent<globalState>();
+        setLevelDifficulty();
+
 
         for (int i = 0; i < lenght; i++)
         {
@@ -24,12 +29,33 @@ public class platformGenerator : MonoBehaviour {
     }
 
     public string randPlatformChooser() {
-        return Random.Range(0, 6).ToString();
+        int lvl = state.level.ToString()[3]-48;         //This line of code will crush after level9 is added!!
+        Debug.Log(lvl);
+        return Random.Range(0,  lvl+3).ToString();
+            
     }
+        
 
+    
     public float randPlatformXLocation() {
         int randNum = (Random.Range(0, 21) - 10);
         return (float)randNum/10;
+    }
+
+
+
+    public void setLevelDifficulty() {
+        if (state.difficulty == globalState.Difficulty.EASY) {
+            lenght = 20;
+        }
+        else if (state.difficulty == globalState.Difficulty.MEDIUM)
+        {
+            lenght = 100;
+        }
+        else if(state.difficulty == globalState.Difficulty.HARD) {
+            lenght = 200;
+        }
+
     }
 
 }
